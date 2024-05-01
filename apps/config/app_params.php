d8ca228c0dcf3b0861fd082619a063b850d9e7b5
Pattanapong Suwan <psuwan@yahoo.com>
2024-05-01 15:54:44 +0700

    Deleted proj_jplanet branch

--- apps/config/app_params.php
+++ apps/config/app_params.php
@@ -6,3 +6,7 @@ $footer_right_menu_01 = 'Footer R01';
 $footer_right_menu_02 = 'Footer R02';
 $footer_right_menu_03 = 'Footer R03';
 $footer_right_menu_04 = 'Footer R04';
+
+$footer_left_menu_01 = 'Footer L01';
+$footer_left_menu_02 = 'Footer L02';
+$footer_left_menu_03 = 'Footer L03';
